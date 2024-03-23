b7f248c78403f0ac4ed52a5a2e91dab46e6343f6
te5154c <techp910@gmail.com>
2024-03-23 19:35:15 +0300

    fix: space pep8 mistake

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,6 +5,7 @@ class FactsResponse(BaseModel):
     status: bool = False
     text: str = "Fact not found"
 
+
 class ChatResponse:
     companion = None
     message_start_id: int = 0
@@ -13,6 +14,6 @@ class ChatResponse:
     # ..but this User class is from the methods.user
     # ..so when I'm import user, it throws "circular import"
     # Need fix to get rid of this shit.
-    def __init__(self, *, companion = None, message_start_id: int = 0) -> None:
+    def __init__(self, *, companion=None, message_start_id: int = 0) -> None:
         self.companion = companion
         self.message_start_id = message_start_id
